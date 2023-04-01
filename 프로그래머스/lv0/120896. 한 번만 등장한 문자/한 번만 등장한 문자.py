0a06d6d199e5fb7d51429ce6596e761e5a61b242
toutelajourn6e <toutelajourn6e@gmail.com>
2023-04-01 20:06:30 +0900

    [level 0] Title: 한 번만 등장한 문자, Time: 0.01 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/lv0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.py
+++ 프로그래머스/lv0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.py
@@ -1,18 +1,2 @@
 def solution(s):
-    answer = dict()
-    my_string = list()
-    for i in s:
-        if not i in answer:
-            answer[i] = 0
-            answer[i] += 1
-        else:
-            answer[i] += 1
-        
-    for j in answer:
-        if answer[j] != 1:
-            pass
-        else:
-            my_string.append(j)
-    my_string.sort()
-    
-    return ''.join(my_string)
+    return ''.join(sorted([i for i in s if s.count(i) == 1]))

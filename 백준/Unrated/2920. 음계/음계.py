a8f5c7aecd2af775b02719ff435cfe522bd6d402
toutelajourn6e <toutelajourn6e@gmail.com>
2023-04-04 19:34:05 +0900

    [Unrated] Title: 음계, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Unrated/2920. 음계/음계.py
+++ 백준/Unrated/2920. 음계/음계.py
@@ -1,13 +1,7 @@
-a = list(map(int, input().split()))
-
-dic = {
-    'ascending': [1,2,3,4,5,6,7,8],
-    'descending': [8,7,6,5,4,3,2,1]   
-}
-
-if a == dic['ascending']:
+a = input()
+if a == '1 2 3 4 5 6 7 8':
     print('ascending')
-elif a == dic['descending']:
+elif a == '8 7 6 5 4 3 2 1':
     print('descending')
 else:
     print('mixed')

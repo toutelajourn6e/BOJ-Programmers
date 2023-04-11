911b0ef6872a34c61631c359cc886a3a876b772d
toutelajourn6e <toutelajourn6e@gmail.com>
2023-04-11 20:12:26 +0900

    [Silver II] Title: 스택 수열, Time: 208 ms, Memory: 32840 KB -BaekjoonHub

---
+++ 백준/Silver/1874. 스택 수열/스택 수열.py
@@ -0,0 +1,36 @@
+import sys
+t = int(sys.stdin.readline())
+
+stack = [0]
+record = list()
+index = 1
+no = False
+for _ in range(t):
+    a = int(sys.stdin.readline())
+    
+    if a > stack[-1]:
+        if index > a:
+            no = True
+            break
+        while not a == stack[-1]:
+            stack.append(index)
+            record.append('+')
+            index += 1
+        stack.pop()
+        record.append('-')
+    elif a < stack[-1]:
+        while not a == stack[-1]:
+            stack.pop()
+            record.append('-')
+        stack.pop()
+        record.append('-')
+    elif a == stack[-1]:
+        stack.pop()
+        record.append('-')
+
+if no == False:
+    for i in record:
+        print(i)
+else:
+    print('NO')
+    

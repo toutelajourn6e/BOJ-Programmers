2e4860d0e829fe75141a83340f73a055a20e5ac4
toutelajourn6e <toutelajourn6e@gmail.com>
2023-04-12 17:25:57 +0900

    [Silver III] Title: 단어 뒤집기 2, Time: 276 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/17413. 단어 뒤집기 2/단어 뒤집기 2.py
@@ -0,0 +1,24 @@
+s = input()
+tag = False
+stack = ''
+
+for i in s:
+    if i == '<':
+        print(stack[::-1], end='')
+        stack = ''
+        tag = True
+        stack += i
+    elif i == '>':
+        tag = False
+        stack += i
+        print(stack, end='')
+        stack = ''
+    elif tag == True:
+        stack += i
+    elif tag == False:
+        if i == ' ':
+            print(stack[::-1] + ' ',end ='')
+            stack = ''
+        else:
+            stack += i
+print(stack[::-1], end='')

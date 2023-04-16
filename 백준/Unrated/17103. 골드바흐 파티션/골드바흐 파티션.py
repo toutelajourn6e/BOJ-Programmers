a3ecd9af63b05ef188876071bfc72c821f09e319
toutelajourn6e <toutelajourn6e@gmail.com>
2023-04-16 16:43:44 +0900

    [Unrated] Title: 골드바흐 파티션, Time: 784 ms, Memory: 42172 KB -BaekjoonHub

---
+++ 백준/Unrated/17103. 골드바흐 파티션/골드바흐 파티션.py
@@ -0,0 +1,31 @@
+import sys
+
+def prime_list(n):
+    seive = [True for i in range(n+1)]
+    m = int(n**0.5)
+    
+    for i in range(2, m+1):
+        if seive[i] == True:
+            j = 2
+            while i*j <= n:
+                seive[i*j] = False
+                j += 1
+    return [i for i in range(2, n+1) if seive[i] == True]
+p = prime_list(1000000)
+s = set(p)
+ans = 0
+
+t = int(sys.stdin.readline().rstrip())
+
+for _ in range(t):
+    n = int(sys.stdin.readline().rstrip())
+    
+    for i in p:
+        if not i <= n//2:
+            break
+            
+        if (n-i) in s:
+            ans += 1
+    print(ans)
+    ans = 0
+            

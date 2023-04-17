92cd51ee77160688e122834e1b1d865e867e64aa
toutelajourn6e <toutelajourn6e@gmail.com>
2023-04-17 20:43:07 +0900

    [unrated] Title: 달리기 경주, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/178871. 달리기 경주/달리기 경주.py
+++ 프로그래머스/unrated/178871. 달리기 경주/달리기 경주.py
@@ -3,12 +3,13 @@ def solution(players, callings):
     grade_rev = {players : i for i, players in enumerate(players)}
 
     for j in callings:
-        call_num = grade_rev[j]
-        call_name = grade[call_num]
+        call_num = grade_rev[j] # 호명 선수 등수
+        call_name = grade[call_num] # 호명 선수 이름
         
-        other_name = grade[call_num-1]
-        other_num = grade_rev[other_name]
+        other_name = grade[call_num-1] # 앞 선수 이름
+        other_num = grade_rev[other_name] # 앞 선수 번호
         
+        # 앞 선수와 호명 선수 (이름, 등수) 교환
         grade_rev[call_name], grade_rev[other_name] = grade_rev[other_name], grade_rev[call_name]
         grade[call_num], grade[other_num] = grade[other_num], grade[call_num]
         

bc3c5ca3710ad648fa7a52cd8056eb64da525d06
toutelajourn6e <toutelajourn6e@gmail.com>
2023-04-20 09:44:46 +0900

    [Silver II] Title: 1, 2, 3 더하기 5, Time: 168 ms, Memory: 51492 KB -BaekjoonHub

--- 백준/Silver/15990. 1， 2， 3 더하기 5/1， 2， 3 더하기 5.py
+++ 백준/Silver/15990. 1， 2， 3 더하기 5/1， 2， 3 더하기 5.py
@@ -1,24 +1,19 @@
-limit = 100000
-d = [[0]*4 for _ in range(limit+1)]
-mod = 1000000009
-for i in range(1, limit+1):
-    if i-1 >= 0:
-        d[i][1] = d[i-1][2] + d[i-1][3]
-        if i == 1:
-            d[i][1] = 1
-    if i-2 >= 0:
-        d[i][2] = d[i-2][1] + d[i-2][3]
-        if i == 2:
-            d[i][2] = 1
-    if i-3 >= 0:
-        d[i][3] = d[i-3][1] + d[i-3][2]
-        if i == 3:
-            d[i][3] = 1
-    d[i][1] %= mod
-    d[i][2] %= mod
-    d[i][3] %= mod
+import sys
 
-t = int(input())
+mod = 1000000009
+d = [[0]*4 for _ in range(100001)]
+d[1] = [0,1,0,0]
+d[2] = [0,0,1,0]
+d[3] = [0,1,1,1]
+
+for i in range(4, 100001):
+    d[i][1] = (d[i-1][2] + d[i-1][3]) % mod
+    d[i][2] = (d[i-2][1] + d[i-2][3]) % mod
+    d[i][3] = (d[i-3][1] + d[i-3][2]) % mod
+    
+t = int(sys.stdin.readline().rstrip())
+
 for _ in range(t):
-    n = int(input())
-    print(sum(d[n])%mod)
+    n = int(sys.stdin.readline().rstrip())
+    print(sum(d[n]) % mod)
+    

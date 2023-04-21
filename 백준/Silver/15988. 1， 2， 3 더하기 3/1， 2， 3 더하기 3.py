a9a99ecf8adc36242a10faf900f379c5fd7f7894
toutelajourn6e <toutelajourn6e@gmail.com>
2023-04-21 14:16:39 +0900

    [Silver II] Title: 1, 2, 3 더하기 3, Time: 492 ms, Memory: 71248 KB -BaekjoonHub

---
+++ 백준/Silver/15988. 1， 2， 3 더하기 3/1， 2， 3 더하기 3.py
@@ -0,0 +1,18 @@
+import sys
+
+d = [0 for _ in range(1000001)]
+d[0] = 1
+d[1] = 1
+d[2] = 2
+mod = 1000000009
+
+for n in range(3, 1000001):
+    d[n] = d[n-1] + d[n-2] + d[n-3]
+    d[n] %= mod
+
+    
+t = int(sys.stdin.readline().rstrip())
+for _ in range(t):
+    n = int(sys.stdin.readline().rstrip())
+    print(d[n])
+    

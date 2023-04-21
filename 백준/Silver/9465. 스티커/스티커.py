8a57a9a3a44d1ecd08fdc1155344fed37aa3efe0
toutelajourn6e <toutelajourn6e@gmail.com>
2023-04-21 17:01:23 +0900

    [Silver I] Title: 스티커, Time: 504 ms, Memory: 229908 KB -BaekjoonHub

--- 백준/Silver/9465. 스티커/스티커.py
+++ 백준/Silver/9465. 스티커/스티커.py
@@ -1,14 +1,15 @@
+import sys
 
-t = int(input())
+t = int(sys.stdin.readline().rstrip())
 
 for _ in range(t):
-    n = int(input())
+    n = int(sys.stdin.readline().rstrip())
     d = [[0,0,0] for _ in range(n+1)]
-    one = [0] + list(map(int, input().split()))
-    two = [0] + list(map(int, input().split()))
+    one = [0] + list(map(int, sys.stdin.readline().rstrip().split()))
+    two = [0] + list(map(int, sys.stdin.readline().rstrip().split()))
     
     for n in range(1, n+1):
-        d[n][0] = max(d[n-1][0], d[n-1][1], d[n-1][2])
+        d[n][0] = max(d[n-1])
         d[n][1] = max(d[n-1][0], d[n-1][2]) + one[n]
         d[n][2] = max(d[n-1][0], d[n-1][1]) + two[n]
         

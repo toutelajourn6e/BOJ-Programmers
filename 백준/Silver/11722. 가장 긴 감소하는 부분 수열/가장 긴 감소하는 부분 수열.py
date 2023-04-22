1f8d6fbd02947d66e30b1c88bdecc97d9b7a2079
toutelajourn6e <toutelajourn6e@gmail.com>
2023-04-22 13:20:04 +0900

    [Silver II] Title: 가장 긴 감소하는 부분 수열, Time: 108 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/11722. 가장 긴 감소하는 부분 수열/가장 긴 감소하는 부분 수열.py
@@ -0,0 +1,11 @@
+n = int(input())
+a = list(map(int, input().split()))
+d = [0] * n
+
+for i in range(n):
+    d[i] = 1
+    for j in range(i+1):
+        if a[j] > a[i] and d[i] < d[j] + 1:
+            d[i] = d[j] + 1
+            
+print(max(d))

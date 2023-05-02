816e23a3cdbf24f7467ab07a28d9742ba2d42522
toutelajourn6e <toutelajourn6e@gmail.com>
2023-05-02 12:23:02 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 4908 ms, Memory: 86168 KB -BaekjoonHub

---
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.py
@@ -0,0 +1,45 @@
+import sys
+sys.setrecursionlimit(10**7)
+
+t = int(sys.stdin.readline().rstrip())
+a = [int(sys.stdin.readline().rstrip()) for _ in range(t)]
+
+def merge_sort(a):
+    n = len(a)
+    if n <= 1:
+        return a
+    mid = n // 2
+    g1 = a[:mid]
+    g2 = a[mid:]
+    
+    merge_sort(g1)
+    merge_sort(g2)
+    
+    i = 0; j = 0; k = 0;
+    
+    while i < len(g1) and j < len(g2):
+        if g1[i] < g2[j]:
+            a[k] = g1[i]
+            i += 1
+            k += 1
+        else:
+            a[k] = g2[j]
+            j += 1
+            k += 1
+            
+    while i < len(g1):
+        a[k] = g1[i]
+        i += 1
+        k += 1
+        
+    while j < len(g2):
+        a[k] = g2[j]
+        j += 1
+        k += 1
+        
+    return a
+
+merge_sort(a)
+
+for l in a:
+    print(l)

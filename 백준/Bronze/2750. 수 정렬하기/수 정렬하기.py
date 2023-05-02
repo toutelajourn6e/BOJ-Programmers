872e0467eb584aef469f6e3dbe0aed9209e97629
toutelajourn6e <toutelajourn6e@gmail.com>
2023-05-02 10:55:40 +0900

    [Bronze II] Title: 수 정렬하기, Time: 80 ms, Memory: 31412 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.py
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.py
@@ -1,14 +1,31 @@
 import sys
+sys.setrecursionlimit(10**7)
 
 n = int(sys.stdin.readline().rstrip())
 a = [int(sys.stdin.readline().rstrip()) for _ in range(n)]
 
-for i in range(n-1):
-    min_idx = i
-    for j in range(i+1, n):
-        if a[j] < a[min_idx]:
-            min_idx = j
-    a[i], a[min_idx] = a[min_idx], a[i]
+def quick_sort_sub(a, start, end):
+    if end - start <= 0: return
     
+    pivot = a[end]
+    i = start
+    
+    for j in range(start, end):
+        if a[j] <= pivot:
+            a[i], a[j] = a[j], a[i]
+            i += 1
+    a[i], a[end] = a[end], a[i]
+    
+    quick_sort_sub(a, start, i - 1)
+    quick_sort_sub(a, i + 1, end)
+    
+    return a
+
+def quick_sort(a):
+    quick_sort_sub(a, 0, len(a)-1)
+
+    
+quick_sort(a)
+
 for k in a:
     print(k)

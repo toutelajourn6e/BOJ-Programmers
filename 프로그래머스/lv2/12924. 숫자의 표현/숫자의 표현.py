a2a277711cece5af8dfc1600d59944ad4885372d
toutelajourn6e <toutelajourn6e@gmail.com>
2023-05-07 13:45:03 +0900

    [level 2] Title: 숫자의 표현, Time: 3.59 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12924. 숫자의 표현/숫자의 표현.py
@@ -0,0 +1,19 @@
+def solution(n):
+    ans = 1
+    for i in range(1, n//2 + 2):
+        temp = i
+        for j in range(i+1, n//2 + 2):
+            temp += j
+            if temp == n:
+                ans += 1
+                break
+            elif temp < n:
+                continue
+            else:
+                break
+    return ans
+            
+            
+                
+                
+            

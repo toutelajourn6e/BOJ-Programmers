0f12ac31e51de0035f36293d6f7d28e35d26419f
toutelajourn6e <toutelajourn6e@gmail.com>
2023-05-11 15:06:54 +0900

    [level 0] Title: 등수 매기기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120882. 등수 매기기/등수 매기기.py
+++ 프로그래머스/lv0/120882. 등수 매기기/등수 매기기.py
@@ -2,8 +2,8 @@ def solution(score):
     ans = []
     avg = []
     
-    for i in score: # 평균 구하기
-        avg.append(sum(i)/len(i))
+    for i,j in score: # 평균 구하기
+        avg.append((i+j)/2)
         
     sort_avg = sorted(avg, reverse=True)
     

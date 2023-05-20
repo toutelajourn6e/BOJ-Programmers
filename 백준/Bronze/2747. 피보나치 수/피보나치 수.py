c2c26e10a27770a847b52b0032a46790d0f06e76
toutelajourn6e <toutelajourn6e@gmail.com>
2023-05-20 13:41:54 +0900

    [Bronze II] Title: 피보나치 수, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/2747. 피보나치 수/피보나치 수.py
@@ -0,0 +1,14 @@
+n = int(input())
+memo = {}
+
+def fibonacci(n):
+    if n <= 1:
+        return n
+    
+    if n in memo:
+        return memo[n]
+    else:
+        memo[n] = fibonacci(n-1) + fibonacci(n-2)
+        return memo[n]
+    
+print(fibonacci(n))

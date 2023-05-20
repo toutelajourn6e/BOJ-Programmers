68d21322a90c6b1e946c42994a8ecd1b9ef58745
toutelajourn6e <toutelajourn6e@gmail.com>
2023-05-20 16:08:50 +0900

    [Gold V] Title: 적록색약, Time: 84 ms, Memory: 34264 KB -BaekjoonHub

---
+++ 백준/Gold/10026. 적록색약/적록색약.py
@@ -0,0 +1,71 @@
+from collections import deque
+
+
+def bfs(x, y, v):
+    q = deque()
+    q.append((x, y))
+    
+    while q:
+        x, y = q.popleft()
+        
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            
+            if 0 <= nx < n and 0 <= ny < n:
+                if not visited[nx][ny] and grid[nx][ny] == v:
+                    visited[nx][ny] = True
+                    q.append((nx, ny))
+                    
+def bfs2(x, y, v):
+    q = deque()
+    q.append((x, y))
+    
+    while q:
+        x, y = q.popleft()
+        
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            
+            if 0 <= nx < n and 0 <= ny < n:
+                if not visited[nx][ny] and grid[nx][ny] == v:
+                    visited[nx][ny] = True
+                    q.append((nx, ny))
+                
+                
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+n = int(input())
+grid = [list(input()) for _ in range(n)]
+visited = [[False] * n for _ in range(n)]
+count1 = 0
+              
+for i in range(n):
+    for j in range(n):
+        if not visited[i][j]:
+            visited[i][j] = True
+            v = grid[i][j]
+            bfs(i, j, v)
+            count1 += 1
+            
+print(count1)
+
+visited = [[False] * n for _ in range(n)]
+count2 = 0
+
+for i in range(n):
+    for j in range(n):
+        if grid[i][j] == 'G':
+            grid[i][j] = 'R'
+
+for i in range(n):
+    for j in range(n):
+        if not visited[i][j]:
+            visited[i][j] = True
+            v = grid[i][j]
+            bfs(i, j, v)
+            count2 += 1
+            
+print(count2)

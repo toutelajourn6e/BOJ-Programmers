60c10f9431ac1626dc72e83f3eeea9b8ea96831d
toutelajourn6e <toutelajourn6e@gmail.com>
2023-05-20 13:04:51 +0900

    [Silver II] Title: N과 M (11), Time: 1552 ms, Memory: 61976 KB -BaekjoonHub

---
+++ 백준/Silver/15665. N과 M （11）/N과 M （11）.py
@@ -0,0 +1,18 @@
+n, m = map(int, input().split())
+a = sorted(list(map(int, input().split())))
+rs = []
+temp = set()
+
+def dfs(depth):
+    if depth == m:
+        if not str(rs) in temp:
+            print(' '.join(map(str, rs)))
+            temp.add(str(rs))
+        return
+    
+    for i in range(n):
+        rs.append(a[i])
+        dfs(depth+1)
+        rs.pop()
+
+dfs(0)

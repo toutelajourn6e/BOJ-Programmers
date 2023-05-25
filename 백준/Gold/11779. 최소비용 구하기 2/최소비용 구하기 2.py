5d00ccedbc075c13ca3c3013e55c99a83e013911
toutelajourn6e <toutelajourn6e@gmail.com>
2023-05-25 12:45:35 +0900

    [Gold III] Title: 최소비용 구하기 2, Time: 292 ms, Memory: 57508 KB -BaekjoonHub

---
+++ 백준/Gold/11779. 최소비용 구하기 2/최소비용 구하기 2.py
@@ -0,0 +1,49 @@
+import heapq
+import sys
+input = sys.stdin.readline
+sys.setrecursionlimit(10**6)
+
+INF = int(1e9)
+n = int(input().rstrip())
+m = int(input().rstrip())
+graph = [[] for _ in range(n+1)]
+distance = [INF] * (n+1)
+
+via = [-1] * (n+1)
+
+for _ in range(m):
+    a, b, c = map(int, input().rstrip().split())
+    graph[a].append((b, c))
+
+start, end = map(int, input().rstrip().split())    
+    
+def dijkstra(start):
+    pq = []
+    heapq.heappush(pq, (0, start))
+    distance[start] = 0
+    
+    while pq:
+        dist, node = heapq.heappop(pq)
+        if distance[node] < dist:
+            continue
+            
+        for adj_node, adj_weight in graph[node]:
+            cost = dist + adj_weight
+            if cost < distance[adj_node]:
+                distance[adj_node] = cost
+                heapq.heappush(pq, (cost, adj_node))
+                via[adj_node] = node
+                
+dijkstra(start)
+print(distance[end])
+
+path = [end]
+
+while end != start:
+    end = via[end]
+    path.append(end)
+    
+path.reverse()
+    
+print(len(path))
+print(*path)

bb476a21a8e037ceb318783d3aff6ada7d11ab42
toutelajourn6e <toutelajourn6e@gmail.com>
2023-06-02 15:34:42 +0900

    [Silver V] Title: 뒤집기, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1439. 뒤집기/뒤집기.py
+++ 백준/Silver/1439. 뒤집기/뒤집기.py
@@ -1,6 +1,13 @@
-import re
 s = input()
-count_1 = len(re.findall('1+', s))
-count_0 = len(re.findall('0+', s))
-
-print(min(count_1, count_0))
+s = s.replace('01', '0x1')
+s = s.replace('10', '1x0')
+s = list(s.split('x'))
+one = 0
+zero = 0
+for i in s:
+    if i[0] == '0':
+        zero += 1
+    else:
+        one += 1
+        
+print(min(one, zero))

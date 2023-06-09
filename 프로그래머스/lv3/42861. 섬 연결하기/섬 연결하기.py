c2cd91cfbc252a9c87e8981761a7e0bd243ddbff
toutelajourn6e <toutelajourn6e@gmail.com>
2023-06-09 21:00:27 +0900

    [level 3] Title: 섬 연결하기, Time: 0.03 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/42861. 섬 연결하기/섬 연결하기.py
@@ -0,0 +1,38 @@
+def find(x, parent):
+    if parent[x] != x:
+        parent[x] = find(parent[x], parent)
+    return parent[x]
+
+def union(a, b, parent):
+    a = find(a, parent)
+    b = find(b, parent)
+    
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
+
+
+def solution(n, costs):
+    parent = [i for i in range(n)]
+    edges = []
+    result = 0
+    
+    for a, b, cost in costs:
+        edges.append((cost, a, b))
+        edges.append((cost, b, a))
+    
+    edges.sort()
+    
+    for cost, a, b in edges:
+        if find(a, parent) != find(b, parent):
+            union(a, b, parent)
+            result += cost
+            
+    return result
+    
+    
+    
+    
+    
+    

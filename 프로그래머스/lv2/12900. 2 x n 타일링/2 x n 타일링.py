47800e0f29dfbd4e13763355617184db1fe7eb76
toutelajourn6e <toutelajourn6e@gmail.com>
2023-06-09 21:25:44 +0900

    [level 2] Title: 2 x n 타일링, Time: 12.10 ms, Memory: 12.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12900. 2 x n 타일링/2 x n 타일링.py
@@ -0,0 +1,10 @@
+def solution(n):
+    mod = 1000000007
+    d = [0] * (n+1)
+    d[0] = d[1] = 1
+    
+    for i in range(2, n+1):
+        d[i] = d[i-1] + d[i-2] % mod
+        
+    return d[n] % mod
+    

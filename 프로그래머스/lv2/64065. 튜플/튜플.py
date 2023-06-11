12d8006372b5cb36decc1dcfa14db3df303abf06
toutelajourn6e <toutelajourn6e@gmail.com>
2023-06-11 13:43:17 +0900

    [level 2] Title: 튜플, Time: 0.02 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv2/64065. 튜플/튜플.py
+++ 프로그래머스/lv2/64065. 튜플/튜플.py
@@ -1,16 +1,14 @@
 def solution(s):
     ans = []
     set_ans = set()
-    s = s.split('},{')
-    s[0] = s[0].replace('{', '')
-    s[-1] = s[-1].replace('}', '')
+    s = s[2:-2].split('},{')
     s.sort(key=lambda x: len(x))
-    
+
     for i in range(len(s)):
         temp = s[i].split(',')
         for j in temp:
             if int(j) not in set_ans:
                 ans.append(int(j))
                 set_ans.add(int(j))
-                
+
     return ans

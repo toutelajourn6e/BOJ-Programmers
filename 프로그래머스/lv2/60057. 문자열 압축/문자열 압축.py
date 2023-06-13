ed2afb706dcbf594448babeea6781a3cba8cc9b7
toutelajourn6e <toutelajourn6e@gmail.com>
2023-06-13 11:30:32 +0900

    [level 2] Title: 문자열 압축, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/60057. 문자열 압축/문자열 압축.py
+++ 프로그래머스/lv2/60057. 문자열 압축/문자열 압축.py
@@ -1,20 +1,35 @@
 def solution(s):
-    ans = len(s)
+    n = len(s)
+    if n == 1:
+        return 1
     
-    for i in range(1, len(s)//2 + 1):
-        start = s[:i]
-        count = 1
-        concat = ''
-        for j in range(i, len(s), i):
-            if start == s[j:j+i]:
-                count += 1
-            else:
-                concat += str(count) + start if count > 1 else start
-                start = s[j:j+i]
-                count = 1
+    ans = 10000
+    
+    for i in range(1, n//2+1):
+        temp = ''
+        cnt = 1
+        word = s[:i]
         
-        concat += str(count) + start if count > 1 else start
-        ans = min(ans, len(concat))
+        for j in range(i, n, i):
+            if s[j:j+i] == word:
+                cnt += 1
+        
+            else:
+                if cnt == 1:
+                    temp += word
+                else:
+                    temp += str(cnt) + word
+                word = s[j:j+i]
+                cnt = 1
+        if cnt == 1:
+            temp += word
+        else:
+            temp += str(cnt) + word
+        ans = min(ans ,len(temp))
         
     return ans
+                
+                
+            
+        
         

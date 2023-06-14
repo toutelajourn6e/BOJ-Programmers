4df3c6354e8fda7e642fe5e22b1ca20940226b79
toutelajourn6e <toutelajourn6e@gmail.com>
2023-06-14 11:42:38 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.06 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.py
+++ 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.py
@@ -1,14 +1,14 @@
-def solution(num):
-    cnt = 0
-    
-    while cnt <= 500:
-        if num == 1:
-            return cnt
-        if num % 2 == 0:
-            num //= 2
-        else:
-            num = (num * 3) + 1
-            
-        cnt += 1
-    else:
+def collatz(n, cnt):
+    if cnt >= 500:
         return -1
+    if n == 1:
+        return cnt
+    
+    if n % 2 == 0:
+        return collatz(n//2, cnt + 1)
+    else:
+        return collatz((n*3)+1, cnt + 1)
+
+
+def solution(num):
+    return collatz(num, 0)

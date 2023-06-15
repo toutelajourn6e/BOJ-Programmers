526e225a41f87686c19af20698a7ff0a0d1c0ec9
toutelajourn6e <toutelajourn6e@gmail.com>
2023-06-15 17:53:41 +0900

    [unrated] Title: A 강조하기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181874. A 강조하기/A 강조하기.py
+++ 프로그래머스/unrated/181874. A 강조하기/A 강조하기.py
@@ -1,8 +1,2 @@
 def solution(myString):
-    ans = ''
-    for i in myString:
-        if i in ['a', 'A']:
-            ans += i.upper()
-        else:
-            ans += i.lower()
-    return ans
+    return myString.lower().replace('a', 'A')

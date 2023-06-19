9ab416ea5b5c1fb6c9dd91dae3b66a236a87a01d
toutelajourn6e <toutelajourn6e@gmail.com>
2023-06-19 18:25:01 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.02 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
+++ 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
@@ -2,7 +2,6 @@ def solution(s):
     nums = ['zero','one','two','three','four','five','six','seven','eight','nine']
     
     for index, num in enumerate(nums):
-        if num in s:
-            s = s.replace(num, str(index))
+        s = s.replace(num, str(index))
             
     return int(s)

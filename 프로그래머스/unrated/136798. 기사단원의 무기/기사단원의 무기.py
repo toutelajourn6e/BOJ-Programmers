874e286efc586bbd76486570bfda7e695c0470f4
toutelajourn6e <toutelajourn6e@gmail.com>
2023-06-22 13:46:30 +0900

    [unrated] Title: 기사단원의 무기, Time: 1.95 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/136798. 기사단원의 무기/기사단원의 무기.py
@@ -0,0 +1,16 @@
+def solution(number, limit, power):
+    result = []
+    
+    for i in range(1, number+1):
+        cnt = 0
+        for j in range(1, int(i**(0.5))+1):
+            if i % j == 0:
+                cnt += 1 
+                if j ** 2 != i:
+                    cnt += 1
+        if cnt > limit:
+            result.append(power)
+        else:
+            result.append(cnt)
+        
+    return sum(result)

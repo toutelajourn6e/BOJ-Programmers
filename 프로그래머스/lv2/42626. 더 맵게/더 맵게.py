17ac506dc55bfdbadf7761ab4ae095a8c1a614a0
toutelajourn6e <toutelajourn6e@gmail.com>
2023-07-01 11:40:42 +0900

    [level 2] Title: 더 맵게, Time: 1948.19 ms, Memory: 51.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42626. 더 맵게/더 맵게.py
@@ -0,0 +1,23 @@
+import heapq as hq
+
+def solution(scoville, K):
+    if K == 0:
+        return 0
+    hq.heapify(scoville)
+    
+    result = 0
+    
+    while len(scoville) > 1:
+        min_v = hq.heappop(scoville)
+        if min_v >= K:
+            return result
+        sec_v = hq.heappop(scoville)
+        if sec_v == 0:
+            return -1
+        hq.heappush(scoville, min_v + (sec_v * 2))
+        result += 1
+        
+    if scoville[0] >= K:
+        return result
+    else:
+        return -1

99f43ccf7d9743530db27db3c57d6269cd01918e
toutelajourn6e <toutelajourn6e@gmail.com>
2023-07-01 12:59:34 +0900

    [level 3] Title: 정수 삼각형, Time: 48.09 ms, Memory: 18.6 MB -BaekjoonHub

--- 프로그래머스/lv3/43105. 정수 삼각형/정수 삼각형.py
+++ 프로그래머스/lv3/43105. 정수 삼각형/정수 삼각형.py
@@ -1,6 +1,6 @@
 def solution(triangle):
     n = len(triangle)
-    dp = [[0] * n for _ in range(n)]
+    dp = [[0] * (i+1) for i in range(1, n+1)]
     dp[0][0] = triangle[0][0]
     
     for i in range(1, n):

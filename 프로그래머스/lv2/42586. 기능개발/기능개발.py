7965d88bc4e4abc383c6b617571b1de3b98ff72d
toutelajourn6e <toutelajourn6e@gmail.com>
2023-07-02 12:01:53 +0900

    [level 2] Title: 기능개발, Time: 0.01 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/lv2/42586. 기능개발/기능개발.py
+++ 프로그래머스/lv2/42586. 기능개발/기능개발.py
@@ -2,22 +2,21 @@ from collections import deque
 
 def solution(progresses, speeds):
     progresses, speeds = deque(progresses), deque(speeds)
-    result, temp = [], 0
+    result, cnt = [], 0
     
     while progresses:
         for idx, speed in zip(range(len(progresses)), speeds):
             progresses[idx] += speed
         
         if progresses[0] >= 100:
-            while True:
-                if len(progresses) == 0 or progresses[0] < 100:
-                    break
+            while progresses and progresses[0] >= 100:
                 progresses.popleft()
                 speeds.popleft()
-                temp += 1
-                
-            result.append(temp)
-            temp = 0
+                cnt += 1
+        
+        if cnt:
+            result.append(cnt)
+            cnt = 0
         
     return result
                 

5e310ca65c39d89347c1c0a7b90f4414cb087a79
toutelajourn6e <toutelajourn6e@gmail.com>
2023-07-03 15:28:06 +0900

    [level 3] Title: 순위, Time: 186.79 ms, Memory: 10.6 MB -BaekjoonHub

--- 프로그래머스/lv3/49191. 순위/README.md
+++ 프로그래머스/lv3/49191. 순위/README.md
@@ -1,10 +1,10 @@
 # [level 3] 순위 - 49191 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/49191) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/49191?language=python3) 
 
 ### 성능 요약
 
-메모리: 10.5 MB, 시간: 198.21 ms
+메모리: 10.6 MB, 시간: 186.79 ms
 
 ### 구분
 
--- 프로그래머스/lv3/49191. 순위/순위.py
+++ 프로그래머스/lv3/49191. 순위/순위.py
@@ -2,11 +2,6 @@ def solution(n, results):
     INF = int(1e9)
     graph = [[INF] * (n+1) for _ in range(n+1)]
     ans = 0
-    
-    for i in range(n+1):
-        for j in range(n+1):
-            if i == j:
-                graph[i][j] = 0
             
     for a, b in results:
         graph[a][b] = 1
@@ -15,6 +10,9 @@ def solution(n, results):
     for k in range(n+1):
         for i in range(n+1):
             for j in range(n+1):
+                if i == j:
+                    graph[i][j] = 0
+                    continue
                 if graph[i][k] == 1 and graph[k][j] == 1:
                     graph[i][j] = 1
                 elif graph[i][k] == -1 and graph[k][j] == -1:

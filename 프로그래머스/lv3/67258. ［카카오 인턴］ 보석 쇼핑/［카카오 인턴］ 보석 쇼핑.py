8738515d2d49dba6b078d74af60c5fa78a7b096a
toutelajourn6e <toutelajourn6e@gmail.com>
2023-07-05 14:45:41 +0900

    [level 3] Title: [카카오 인턴] 보석 쇼핑, Time: 70.07 ms, Memory: 17.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.py
@@ -0,0 +1,26 @@
+def solution(gems):
+    kind = len(set(gems))
+    n = len(gems)
+    ans = [0, n-1]
+    
+    dic = {gems[0]:1}
+    left, right = 0, 0
+    
+    while right < n:
+        if len(dic) < kind:
+            right += 1
+            if right == n:
+                break
+            dic[gems[right]] = dic.get(gems[right], 0) + 1
+        else:
+            if (right - left + 1) < (ans[1] - ans[0] + 1):
+                ans = [left, right]
+            if dic[gems[left]] == 1:
+                del dic[gems[left]]
+            else:
+                dic[gems[left]] -= 1
+            left += 1
+    ans[0] += 1
+    ans[1] += 1
+            
+    return ans

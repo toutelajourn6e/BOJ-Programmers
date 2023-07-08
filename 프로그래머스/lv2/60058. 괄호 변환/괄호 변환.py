429b250397dcf03dd0f25bcd70744534b3574ae8
toutelajourn6e <toutelajourn6e@gmail.com>
2023-07-08 14:36:02 +0900

    [level 2] Title: 괄호 변환, Time: 0.22 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/60058. 괄호 변환/괄호 변환.py
@@ -0,0 +1,44 @@
+def check(w):
+    stack = []
+    for i in w:
+        if i == '(':
+            stack.append(i)
+        else:
+            if not stack:
+                return False
+            if stack[-1] == '(':
+                stack.pop()
+    return False if stack else True
+                
+def solution(p):
+    if p == '':
+        return ''
+    result = []
+    
+    def separate(w):
+        u, v = '', ''
+        l_cnt = r_cnt = 0
+        
+        for i in range(len(w)):
+            if w[i] == '(': l_cnt += 1
+            else: r_cnt += 1
+            u += w[i]
+            if l_cnt == r_cnt:
+                v = w[i+1:]
+                break
+        
+        if check(u):
+            if v: u += separate(v)
+        else:
+            temp = '('
+            temp += separate(v)
+            temp += ')'
+            u = u[1:-1]
+            u = list(u)
+            for i in range(len(u)):
+                if u[i] == '(': u[i] = ')'
+                else: u[i] = '('
+            u = temp + ''.join(u)
+        return u
+    
+    return separate(p)

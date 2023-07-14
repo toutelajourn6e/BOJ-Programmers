0f43151e9c6d55ba5d1bdd11880dcff896e50ddb
toutelajourn6e <toutelajourn6e@gmail.com>
2023-07-14 14:22:17 +0900

    [level 1] Title: 신고 결과 받기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.py
@@ -0,0 +1,22 @@
+from collections import defaultdict
+
+def solution(id_list, report, k):
+    result = []
+    report_cnt = defaultdict(set)
+    
+    for content in report:
+        send, receive = content.split()
+        report_cnt[receive].add(send)
+        
+    notice = defaultdict(int)
+    for user in report_cnt:
+        if len(report_cnt[user]) >= k:
+            for i in report_cnt[user]:        
+                notice[i] += 1
+                
+    for i in range(len(id_list)):
+        id_list[i] = notice[id_list[i]]
+        
+    return id_list
+    
+    

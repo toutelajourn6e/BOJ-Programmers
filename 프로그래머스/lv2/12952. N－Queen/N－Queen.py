678b2db55a19a3cdc4d27d0707a1658a3291edd0
toutelajourn6e <toutelajourn6e@gmail.com>
2023-07-29 13:45:40 +0900

    [level 2] Title: N-Queen, Time: 1812.39 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12952. N－Queen/N－Queen.py
@@ -0,0 +1,20 @@
+def solution(n):
+    ans = 0
+    
+    def n_queen(row, col, left, right):
+        nonlocal ans
+        if row == n:
+            ans += 1
+            return
+        
+        left <<= 1
+        right >>= 1
+        impossible = col | left | right
+        
+        for i in range(n):
+            if impossible & 1 << i: continue
+            bit = 1 << i
+            n_queen(row+1, col | bit, left | bit, right | bit)
+        
+    n_queen(0, 0, 0, 0)
+    return ans

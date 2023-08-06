9d758c17059477bb5c4e925ebcbf1a016851415b
toutelajourn6e <toutelajourn6e@gmail.com>
2023-08-06 12:57:54 +0900

    [level 2] Title: 입양 시각 구하기(1), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv2/59412. 입양 시각 구하기（1）/입양 시각 구하기（1）.sql
+++ 프로그래머스/lv2/59412. 입양 시각 구하기（1）/입양 시각 구하기（1）.sql
@@ -2,5 +2,5 @@
 SELECT hour(datetime) as hour, count(*) as count
 from animal_outs
 where hour(datetime) between 9 and 19
-group by hour(datetime)
-order by hour(datetime)
+group by hour
+order by hour

602a5642b50ca5e033a18e5b4b9ca602198268fd
toutelajourn6e <toutelajourn6e@gmail.com>
2023-08-06 12:53:47 +0900

    [level 2] Title: 중성화 여부 파악하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/59409. 중성화 여부 파악하기/중성화 여부 파악하기.sql
@@ -0,0 +1,7 @@
+-- 코드를 입력하세요
+SELECT animal_id, name, 
+    case when SEX_UPON_INTAKE like ('%Neutered%') 
+    or SEX_UPON_INTAKE like ('%Spayed%') then 'O'
+        else 'X' end as SEX_UPON_INTAKE
+from animal_ins
+order by animal_id

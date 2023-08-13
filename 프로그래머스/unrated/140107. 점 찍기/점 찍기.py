8583942b9394e2cabdf35d52ecbe9bf97f200ede
toutelajourn6e <toutelajourn6e@gmail.com>
2023-08-13 13:20:07 +0900

    [unrated] Title: 점 찍기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/140107. 점 찍기/점 찍기.py
@@ -0,0 +1,11 @@
+from math import sqrt
+
+def solution(k, d):
+    ans = 0
+    nums = [i * k for i in range(d+1) if i * k <= d]
+
+    for x in nums:
+        y = int(((d ** 2) - (x ** 2)) ** 0.5)
+        ans += (y // k) + 1
+    
+    return ans

6835f672f79717715f1afe53cabd6570155f73c5
toutelajourn6e <toutelajourn6e@gmail.com>
2023-08-17 13:03:21 +0900

    [level 3] Title: 가장 긴 팰린드롬, Time: 1.25 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv3/12904. 가장 긴 팰린드롬/가장 긴 팰린드롬.py
+++ 프로그래머스/lv3/12904. 가장 긴 팰린드롬/가장 긴 팰린드롬.py
@@ -3,8 +3,8 @@ def solution(s):
     
     for i in range(len(s)):
         for j in range(len(s), -1, -1):
+            if j - i < ans: break
             string = s[i:j]
             if string == string[::-1]:
                 ans = max(ans, len(string))
-                
     return ans

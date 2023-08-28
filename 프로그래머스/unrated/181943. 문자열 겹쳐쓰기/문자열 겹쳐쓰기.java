6723cb8b4c773e3f0919396fb930cc9c21d6ad4a
toutelajourn6e <toutelajourn6e@gmail.com>
2023-08-28 16:22:21 +0900

    [unrated] Title: 문자열 겹쳐쓰기, Time: 14.08 ms, Memory: 84 MB -BaekjoonHub

--- 프로그래머스/unrated/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.java
+++ 프로그래머스/unrated/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.java
@@ -1,16 +1,7 @@
 class Solution {
     public String solution(String my_string, String overwrite_string, int s) {
-        String answer = "";
-        
-        for (int i = 0; i < my_string.length(); i++){
-            if (i == s){
-                answer += overwrite_string;
-                i += overwrite_string.length() - 1;
-            }
-            else
-                answer += my_string.charAt(i);
-        }
-        
-        return answer;
+        String before = my_string.substring(0, s);
+        String after = my_string.substring(s + overwrite_string.length());
+        return before + overwrite_string + after;
     }
 }

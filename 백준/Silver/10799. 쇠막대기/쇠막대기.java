451fdf15c57e0667bda8e4f973419708a4b224d1
toutelajourn6e <toutelajourn6e@gmail.com>
2024-03-14 15:05:59 +0900

    [Silver II] Title: 쇠막대기, Time: 156 ms, Memory: 15752 KB -BaekjoonHub

--- 백준/Silver/10799. 쇠막대기/쇠막대기.java
+++ 백준/Silver/10799. 쇠막대기/쇠막대기.java
@@ -1,26 +1,24 @@
-import java.util.*;
 import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        Deque<Character> stack = new ArrayDeque<>();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         
         String input = br.readLine();
         boolean laserOn = false;
-        int count = 0;
+        int stack = 0, count = 0;
         
         for (char c : input.toCharArray()) {
             if (c == '(') {
                 laserOn = true;
-                stack.push('(');
+                stack++;
             } else {
                 if (laserOn) {
-                    stack.pop();
-                    count += stack.size();
+                    stack--;
+                    count += stack;
                     laserOn = false;
                 } else {
-                    stack.pop();
+                    stack--;
                     count++;
                 }
             }

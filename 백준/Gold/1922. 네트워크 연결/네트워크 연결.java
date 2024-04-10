7780595f27c280c8b04f6598e63145c703d4bdd6
toutelajourn6e <toutelajourn6e@gmail.com>
2024-04-10 23:36:51 +0900

    [Gold IV] Title: 네트워크 연결, Time: 536 ms, Memory: 47116 KB -BaekjoonHub

---
+++ 백준/Gold/1922. 네트워크 연결/네트워크 연결.java
@@ -0,0 +1,74 @@
+import java.io.*;
+import java.util.*;
+import java.util.stream.IntStream;
+
+class Edge implements Comparable<Edge> {
+	int a;
+	int b;
+	int cost;
+
+	Edge(int a, int b, int cost) {
+		this.a = a;
+		this.b = b;
+		this.cost = cost;
+	}
+
+	@Override
+	public int compareTo(Edge another) {
+		return Integer.compare(this.cost, another.cost);
+	}
+}
+
+
+public class Main {
+	static Queue<Edge> pq = new PriorityQueue<>();
+	static int[] parent;
+
+    public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int N = Integer.parseInt(br.readLine());
+		int M = Integer.parseInt(br.readLine());
+
+		parent = IntStream.rangeClosed(0, N).toArray();
+
+		for (int i = 0; i < M; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			int cost = Integer.parseInt(st.nextToken());
+			pq.add(new Edge(a, b, cost));
+		}
+
+		int total = 0;
+		while (!pq.isEmpty()) {
+			Edge curEdge = pq.poll();
+			int a = curEdge.a;
+			int b = curEdge.b;
+			int cost = curEdge.cost;
+
+			if (find(a) != find(b)) {
+				union(a, b);
+				total += cost;
+			}
+		}
+
+		System.out.println(total);
+	}
+
+
+	static int find(int x) {
+		if (parent[x] == x) return x;
+		else return find(parent[x]);
+	}
+
+	static void union(int a, int b) {
+		int parentA = find(a);
+		int parentB = find(b);
+
+		if (parentA < parentB) parent[parentB] = parentA;
+		else parent[parentA] = parentB;
+	}
+
+
+}
